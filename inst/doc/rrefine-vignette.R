e7306cc8992af4c0183cbc6be2d19990f9f28447
VP Nagraj <nagraj@nagraj.net>
2021-11-14 13:40:02 +0000

    version 2.0.0

--- inst/doc/rrefine-vignette.R
+++ inst/doc/rrefine-vignette.R
@@ -2,8 +2,13 @@
 library(rrefine)
 
 ## ---- echo=TRUE, eval = FALSE-------------------------------------------------
+#  install.packages("rrefine")
+
+## ---- echo=TRUE, eval = FALSE-------------------------------------------------
 #  # install.packages("devtools")
 #  devtools::install_github("vpnagraj/rrefine")
+
+## ---- echo=TRUE, eval = FALSE-------------------------------------------------
 #  library(rrefine)
 
 ## ---- echo=FALSE, results='asis'----------------------------------------------
@@ -12,6 +17,20 @@ knitr::kable(lateformeeting)
 ## ---- eval = FALSE, echo = TRUE, warning = FALSE, message=FALSE---------------
 #  write.csv(lateformeeting, file = "lateformeeting.csv", row.names = FALSE)
 #  refine_upload(file = "lateformeeting.csv", project.name = "lfm_cleanup", open.browser = TRUE)
+
+## ---- eval = FALSE, echo = TRUE, warning = FALSE, message=FALSE---------------
+#  refine_add_column(new_column = "dotw_allcaps",
+#                    base_column = "what.day.whas.it",
+#                    value = "grel:value",
+#                    project.name = "lfm_cleanup")
+#  refine_to_upper(column_name = "dotw_allcaps", project.name = "lfm_cleanup")
+#  refine_export(project.name = "lfm_cleanup")$dotw_allcaps
+
+## ----echo=FALSE---------------------------------------------------------------
+toupper(lfm_clean$dotw)
+
+## ---- eval = FALSE, echo = TRUE, warning = FALSE, message=FALSE---------------
+#  refine_remove_column(column = "dotw_allcaps", project.name = "lfm_cleanup")
 
 ## ---- eval = FALSE, echo = TRUE, warning = FALSE, message=FALSE---------------
 #  lfm_clean <- refine_export(project.name = "lfm_cleanup")
